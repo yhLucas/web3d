89379415566f1bb5a3d02001014d786870c9d496
yicongyuan <yicongyuan@tencent.com>
2022-06-18 16:46:28 +0800

    游戏场景引入vue

--- frontend/src/views/VirtualScene.vue
+++ frontend/src/views/VirtualScene.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <iframe src="/index.html">
-    </iframe>
-  </div>
+    <iframe src="/ThreeJs/index.html" id="game-frame"></iframe>
 </template>
+<style scoped>
+#game-frame{
+  overflow-scrolling: unset;
+  width: 98%;
+  height: 98%;
+  padding: 0;
+  margin: 0;
+}
+</style>
